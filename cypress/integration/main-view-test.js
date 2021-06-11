3d904eec6f674eebe0b0df693bb12ee67c4ef9fa
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-11 17:05:57 -0600

    update form validation

--- cypress/integration/main-view-test.js
+++ cypress/integration/main-view-test.js
@@ -1,7 +1,18 @@
-describe('emotion-test-view', () => {
+describe('Emotion Question View', () => {
 
 
   beforeEach(() => {
+    cy.stubCocktails()
+  });
 
-  })
+  describe('Page Load User Flow', () => {
+
+    it('should be able to visit the page and render the correct elements', () => {
+      cy.get('.load')
+        .contains('Loading...')
+        .get('h1')
+        .contains('Mixed Drinks About Feelings')
+    });
+  });
+
 });
