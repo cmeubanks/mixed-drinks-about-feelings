ba2cc719b63ac1c4613bd1cec6e4ff43bca3a7a9
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-11 09:34:13 -0600

    conditional rendering for drink on load

--- src/components/Drink/Drink.js
+++ src/components/Drink/Drink.js
@@ -4,9 +4,10 @@ import './Drink.css';
 const Drink = ({drink}) => {
   return (
     <article className='drink' id={drink.idDrink}>
+    {!drink && <p>Select Your Mood</p>}
       <h2>{drink.strDrink}</h2>
       <div className='bev-img'>
-        <img src={drink.strDrinkThumb} alt='drink' />
+        {drink && <img src={drink.strDrinkThumb} alt='drink' />}
       </div>
     </article>
   )
