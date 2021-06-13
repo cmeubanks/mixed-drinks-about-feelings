4d1a0a4ca3c7aabfe8c21f5b3654b4e926b749a9
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-13 13:25:34 -0600

    create Drink Proptypes

--- src/components/Drink/Drink.js
+++ src/components/Drink/Drink.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import './Drink.css';
 
 const Drink = ({drink, favoriteDrink}) => {
+  console.log(drink)
   return (
     <article className='drink' id={drink.idDrink}>
       <h2>{drink.strDrink}</h2>
@@ -14,3 +16,12 @@ const Drink = ({drink, favoriteDrink}) => {
 }
 
 export default Drink;
+
+Drink.propTypes = {
+  favoriteDrink: PropTypes.func.isRequired,
+  drink: PropTypes.shape({
+   idDrink: PropTypes.string,
+   strDrink: PropTypes.string,
+   strDrinkThumb: PropTypes.string
+ }).isRequired
+}
